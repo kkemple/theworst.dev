7abc871ec338f4e20068a55d2c7d37a2a746fcbf
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-09 16:06:42 -0500

    add service worker to kick old gatsby one

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,26 @@ import "@styles/code-theme.css";
 
 import withDarkMode from "next-dark-mode";
 import Layout from "@components/Layout";
+import { useEffect } from "react";
 
 function Application({ Component, pageProps, darkMode }) {
   const { darkModeActive } = darkMode;
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("./sw.js", { scope: "." })
+        .then((reg) => {
+          // registration worked
+          console.log("Registration succeeded. Scope is " + reg.scope);
+        })
+        .catch((error) => {
+          // registration failed
+          console.log("Registration failed with " + error);
+        });
+    }
+  }, []);
+
   return (
     <Layout darkModeActive={darkModeActive} {...pageProps}>
       <Component {...pageProps} />
