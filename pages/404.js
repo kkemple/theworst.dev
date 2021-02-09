c619bea2548e692f3eef46db672b95b674978a91
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-09 13:07:28 -0500

    add basic 404

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "./404.module.css";
+
+export default function FourOhFour() {
+  return (
+    <main className={styles.container}>
+      <h1>404 - Page Not Found</h1>
+    </main>
+  );
+}
--- pages/content.js
+++ pages/content.js
@@ -2,28 +2,14 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./content.module.css";
 
-const shuffle = (arr) => {
-  const newArr = arr.slice();
-  for (let i = newArr.length - 1; i > 0; i--) {
-    const rand = Math.floor(Math.random() * (i + 1));
-    [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
-  }
-  return newArr;
-};
-
 export default function Garden({ posts }) {
-  const [shuffled, setShuffled] = useState([]);
   const [filter, setFilter] = useState("");
-
-  useEffect(() => {
-    setShuffled(shuffle([...posts]));
-  }, [posts]);
 
   const handleSearch = (event) => {
     setFilter(event.target.value);
   };
 
-  const results = shuffled.filter((post) => {
+  const results = posts.filter((post) => {
     return post.title.toLowerCase().includes(filter.toLowerCase());
   });
 
@@ -56,6 +42,15 @@ export default function Garden({ posts }) {
 }
 
 export async function getStaticProps() {
+  const shuffle = (arr) => {
+    const newArr = arr.slice();
+    for (let i = newArr.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+    }
+    return newArr;
+  };
+
   function importAll(r) {
     return r.keys().map((fileName) => ({
       link: fileName.substr(1).replace(/\/index\.mdx$/, ""),
@@ -64,13 +59,15 @@ export async function getStaticProps() {
   }
 
   const postsData = importAll(require.context(".", true, /\.mdx$/));
-  const posts = postsData.map((postData) => {
-    return {
-      title: postData.module.meta.title,
-      slug: postData.link.replace(".mdx", ""),
-      description: postData.module.meta?.description,
-    };
-  });
+  const posts = shuffle(
+    postsData.map((postData) => {
+      return {
+        title: postData.module.meta.title,
+        slug: postData.link.replace(".mdx", ""),
+        description: postData.module.meta?.description,
+      };
+    })
+  );
 
   return {
     props: {
