f7adf5240aac8080206c1b579f1a72e94ce6976f
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-09 13:17:17 -0500

    add 404 and new post

--- pages/404.js
+++ pages/404.js
@@ -5,6 +5,7 @@ export default function FourOhFour() {
   return (
     <main className={styles.container}>
       <h1>404 - Page Not Found</h1>
+      <img src="/kakashi-disappear.gif" alt="Kakashi disappearing" />
     </main>
   );
 }
