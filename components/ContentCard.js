08f1f89f6b40045634d01921d0b5e411722e8795
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-10 14:49:48 -0500

    remove title from some links

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -9,7 +9,7 @@ export default function ContentCard({ url, title, description }) {
     <div className={styles.container}>
       <div className={styles.content}>
         <Link href={url}>
-          <a onClick={playEnter} title={title}>
+          <a onClick={playEnter}>
             <h5>{title}</h5>
             <p>{description}</p>
           </a>
