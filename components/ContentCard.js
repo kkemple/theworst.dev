7150ee973a553cb5fc80b63ddcca60e440b4ee09
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-10 14:34:15 -0500

    add sound when user clicks on post

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import styles from "./ContentCard.module.css";
+import useSound from "use-sound";
 
 export default function ContentCard({ url, title, description }) {
+  const [playEnter] = useSound("/enter.mp3", { volume: 0.3 });
+
   return (
-    <div className={styles.container}>
+    <div onClick={playEnter} className={styles.container}>
       <div className={styles.content}>
         <Link href={url}>
           <a title={title}>
