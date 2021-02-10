4aee7b3b3978e08eb5487d7b918c71aa8eb17a74
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-10 14:42:18 -0500

    adjust audio

--- components/ContentCard.js
+++ components/ContentCard.js
@@ -6,10 +6,10 @@ export default function ContentCard({ url, title, description }) {
   const [playEnter] = useSound("/enter.mp3", { volume: 0.3 });
 
   return (
-    <div onClick={playEnter} className={styles.container}>
+    <div className={styles.container}>
       <div className={styles.content}>
         <Link href={url}>
-          <a title={title}>
+          <a onClick={playEnter} title={title}>
             <h5>{title}</h5>
             <p>{description}</p>
           </a>
