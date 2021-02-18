e648f956fd7bb7b2deb1512a307e227af61cdd0f
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-17 20:59:25 -0500

    fix realtime issues with cache

--- website/utils/graphql/operation-policies.js
+++ website/utils/graphql/operation-policies.js
@@ -1,16 +1,27 @@
+import { gql } from "@apollo/client";
+
 const operationPolicies = {
   Post: {
     events: {
       // subscribe to the POST_UPDATED event on the Post query
       // example:
       // query Post($slug: String!) @_live(events: [POST_UPDATED]) {...}
-      POST_UPDATED(cache, data, query) {
-        cache.writeQuery({
-          query,
+      POST_UPDATED(cache, data) {
+        const id = cache.identify({
+          ...data,
+          __typename: "Post",
+        });
+
+        cache.writeFragment({
+          id,
           data,
-          variables: {
-            slug: data.slug,
-          },
+          fragment: gql`
+            fragment PostUpdated on Post {
+              id
+              count
+              slug
+            }
+          `,
         });
       },
     },
