614fb50d3471d402887014e9686170f2f94d3e21
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-18 14:05:41 -0500

    update styles for block quotes

--- website/utils/graphql/operation-policies.js
+++ website/utils/graphql/operation-policies.js
@@ -7,19 +7,20 @@ const operationPolicies = {
       // example:
       // query Post($slug: String!) @_live(events: [POST_UPDATED]) {...}
       POST_UPDATED(cache, data) {
+        // let Apollo cache figure out which item in the cache to update
         const id = cache.identify({
           ...data,
           __typename: "Post",
         });
 
+        // get post with key Post:<post_id>
+        // update the count of post likes
         cache.writeFragment({
           id,
           data,
           fragment: gql`
             fragment PostUpdated on Post {
-              id
               count
-              slug
             }
           `,
         });
