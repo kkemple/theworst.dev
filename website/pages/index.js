5bd6b2bef53802453247724549c1af0298a3c4e6
Kurt Kemple <kurtiskemple@gmail.com>
2021-10-24 15:35:46 -0400

    update recent appearances

--- website/pages/index.js
+++ website/pages/index.js
@@ -64,7 +64,34 @@ export default function Home() {
         <h3>Some of my recent appearances</h3>
         <div className={styles.appearances}>
           <div className={`${styles.postCard} ${styles.no_hover}`}>
+            <h5>Business Insider Article</h5>
+            <p>
+              I had the privilege of sharing my story of incarceration and
+              breaking into tech with{" "}
+              <a href="https://www.businessinsider.com/author/kylie-robison">
+                Kylie Robison
+              </a>
+              , a journalist for Business Insider focused on developers and
+              their communities, tools, startups, and more.
+            </p>
+            <Link href="https://www.businessinsider.com/aws-jail-formerly-incarcrated-kurt-kemple-developer-advocate-2021-4">
+              <a>
+                <img
+                  width="100%"
+                  src="/bi-article.png"
+                  alt="This former felon has a successful career in tech with stints at Amazon and hot startups — and he wants to teach other ex-cons how to do it too"
+                />
+              </a>
+            </Link>
+          </div>
+          <div className={`${styles.postCard} ${styles.no_hover}`}>
             <h5>#AntiracistEconomist Podcast</h5>
+            <p>
+              I had the honor of being a guest on{" "}
+              <a href="https://twitter.com/KimCrayton1">Kim Crayton's</a>{" "}
+              podcast where we discussed white privilege, the prison industrial
+              complex, and the issues of mediocre white men in tech.
+            </p>
             <Link href="https://hashtagcauseascene.com/podcast/kurt-kemple/">
               <a>
                 <img
@@ -81,11 +108,13 @@ export default function Home() {
           </div>
           <div className={`${styles.postCard} ${styles.no_hover}`}>
             <h5>Learn with Jason x freeCodeCamp Interview</h5>
+            <p>
+              I spoke with long time friend{" "}
+              <a href="https://twitter.com/jlengstorf">Jason Lengstorf</a> about
+              my journey into tech. The main focus was on applying skills gained
+              from other professions to the tech industry.
+            </p>
             <YouTube youTubeId="4fq8QlpEMec" />
-          </div>
-          <div className={`${styles.postCard} ${styles.no_hover}`}>
-            <h5>Landing in Tech Podcast</h5>
-            <SoundCloud soundCloudLink="tracks/948787954" color="f272ad" />
           </div>
         </div>
       </main>
