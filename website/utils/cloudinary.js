c9d3992a26119e0f2131491278cfd5c57fdf9ddb
Kurt Kemple <kurtiskemple@gmail.com>
2021-11-25 17:22:38 -0500

    change cloudinary base template

--- website/utils/cloudinary.js
+++ website/utils/cloudinary.js
@@ -1,5 +1,5 @@
 export const buildCloudinaryURL = (title) => {
-  const url = `https://res.cloudinary.com/theworstdev/image/upload/l_Gradient/l_text:Montserrat_64_black:${title},c_fit,g_center,w_800,fl_cutter,fl_layer_apply/v1637878438/social-card-twd_szqlqy.png`;
+  const url = `https://res.cloudinary.com/theworstdev/image/upload/l_Gradient/l_text:Montserrat_64_black:${title},c_fit,g_center,w_800,fl_cutter,fl_layer_apply/v1637878922/social-card-twd_ahsoib.png`;
 
   fetch(url);
 
