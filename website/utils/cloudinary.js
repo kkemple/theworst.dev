b18f5ef4b82ed7be3f90212a06b8d50379e03115
Kurt Kemple <kurtiskemple@gmail.com>
2021-11-25 21:24:14 -0500

    remove branding from social images

--- website/utils/cloudinary.js
+++ website/utils/cloudinary.js
@@ -1,7 +1,9 @@
 export const buildCloudinaryURL = (title) => {
-  const url = `https://res.cloudinary.com/theworstdev/image/upload/l_Gradient/l_text:Montserrat_64_black:${title},c_fit,g_center,w_800,fl_cutter,fl_layer_apply/v1637878922/social-card-twd_ahsoib.png`;
+  const url = `https://res.cloudinary.com/theworstdev/image/upload/l_Gradient/l_text:Montserrat_64_black:${title},c_fit,g_center,w_800,fl_cutter,fl_layer_apply/Background`;
 
   fetch(url);
 
   return url;
 };
+
+https://res.cloudinary.com/theworstdev/image/upload/l_Gradient/l_text:Montserrat_64_black:Test,c_fit,g_center,w_800,fl_cutter,fl_layer_apply/Background
