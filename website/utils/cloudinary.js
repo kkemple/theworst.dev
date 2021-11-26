5be13c982c4efffc7b34caa1515bf5f0ca0822d3
Kurt Kemple <kurtiskemple@gmail.com>
2021-11-25 21:26:50 -0500

    update url

--- website/utils/cloudinary.js
+++ website/utils/cloudinary.js
@@ -1,5 +1,5 @@
 export const buildCloudinaryURL = (title) => {
-  const url = `https://res.cloudinary.com/theworstdev/image/upload/l_Gradient/l_text:Montserrat_64_black:${title},c_fit,g_center,w_800,fl_cutter,fl_layer_apply/Background`;
+  const url = `https://res.cloudinary.com/theworstdev/image/upload/l_Gradient/l_text:Montserrat_64_black:${title},c_fit,g_center,w_800,fl_cutter,fl_layer_apply/v1637893282/Background.png`;
 
   fetch(url);
 
