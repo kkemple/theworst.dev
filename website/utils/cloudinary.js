394f04869f0fa6f0e6f447956963b58fee67fa74
Kurt Kemple <kurtiskemple@gmail.com>
2021-11-25 21:24:34 -0500

    remove extra url

--- website/utils/cloudinary.js
+++ website/utils/cloudinary.js
@@ -5,5 +5,3 @@ export const buildCloudinaryURL = (title) => {
 
   return url;
 };
-
-https://res.cloudinary.com/theworstdev/image/upload/l_Gradient/l_text:Montserrat_64_black:Test,c_fit,g_center,w_800,fl_cutter,fl_layer_apply/Background
