acd15176efa5024881326a8f325b52a0e31db444
Kurt Kemple <kurtiskemple@gmail.com>
2024-06-19 10:34:23 -0400

    Update Bio.js

--- components/Bio.js
+++ components/Bio.js
@@ -16,9 +16,9 @@ export default function Bio() {
       </p>
       <hr />
       <p className={styles.bio}>
-        Kurtis Kemple is a tech professional currently working as a Sr. Director of Developer Growth & Engagement at Slack. In addition to his work in tech, Kurtis is an avid amateur photographer. He enjoys capturing the world around him through his lens and sharing his unique perspectives via street photography and photojournalism.<br/>
+        Kurtis Kemple is a tech professional currently working as a Sr. Director of Developer Growth & Engagement at Slack. In addition to his work in tech, Kurtis is an avid amateur photographer. He enjoys capturing the world around him through his lens and sharing his unique perspectives via street photography and photojournalism.<be /><br/>
 
-        As a formerly incarcerated individual, Kurtis brings resilience, adaptability, and a commitment to positive change to everything he does. He is dedicated to using his skills and experiences to make a meaningful impact.<br />
+        As a formerly incarcerated individual, Kurtis brings resilience, adaptability, and a commitment to positive change to everything he does. He is dedicated to using his skills and experiences to make a meaningful impact.<br /><br />
 
         Through his multifaceted background, Kurtis offers valuable insights into the intersection of technology, creativity, and social change. He is always eager to connect with others and explore new opportunities to learn, grow, and contribute to the community.
       </p>
