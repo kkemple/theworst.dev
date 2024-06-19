5d6e2e280525fcaf2ef5d572fb6a74d4be4970cb
Kurt Kemple <kurtiskemple@gmail.com>
2024-06-19 09:55:29 -0400

    Update Bio.js

--- components/Bio.js
+++ components/Bio.js
@@ -4,26 +4,23 @@ export default function Bio() {
   return (
     <div className={styles.container}>
       <p className={styles.bio}>
-        Kurt Kemple, Founder and Principal Advisor at Forthright
+        Kurtis Kemple, Sr. Director of Developer Growth & Engagement, Slack
       </p>
       <hr />
       <p className={styles.bio}>
-        Kurt Kemple is a multidisciplinary tech leader, engineer, and teacher.
+        Kurtis Kemple is a tech professional currently working as a Sr. Director of Developer Growth & Engagement at Slack. In addition to his work in tech, Kurtis is an avid amateur photographer.
       </p>
       <hr />
       <p className={styles.bio}>
-        Kurt Kemple is a multidisciplinary tech leader, engineer, and teacher.
-        He's the creator of The Developer Advocate's Guide series, Developer
-        Experience Audits, and the Developer Advocacy Value Cycle.
+        Kurtis Kemple is a tech professional currently working as a Sr. Director of Developer Growth & Engagement at Slack. In addition to his work in tech, Kurtis is an avid amateur photographer. He enjoys capturing the world around him through his lens and sharing his unique perspectives via street photography and photojournalism.
       </p>
       <hr />
       <p className={styles.bio}>
-        Kurt Kemple is a multidisciplinary tech leader, engineer, and teacher.
-        He's the creator of The Developer Advocate's Guide series, Developer
-        Experience Audits, and the Developer Advocacy Value Cycle. He's led
-        developer relations and engineering teams at companies like Apollo, AWS,
-        and MLS and is now focused on helping others create better developer
-        experiences.
+        Kurtis Kemple is a tech professional currently working as a Sr. Director of Developer Growth & Engagement at Slack. In addition to his work in tech, Kurtis is an avid amateur photographer. He enjoys capturing the world around him through his lens and sharing his unique perspectives via street photography and photojournalism.
+
+        As a formerly incarcerated individual, Kurtis brings resilience, adaptability, and a commitment to positive change to everything he does. He is dedicated to using his skills and experiences to make a meaningful impact.
+
+        Through his multifaceted background, Kurtis offers valuable insights into the intersection of technology, creativity, and social change. He is always eager to connect with others and explore new opportunities to learn, grow, and contribute to the community.
       </p>
     </div>
   );
